d834562c4cd0727fbcfd9dc0ce967fedfc7f764c
akgoode <akgoode@gmail.com>
2017-07-29 15:59:25 -0500

    added hashrouter to index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { HashRouter, Route, Link } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+  <HashRouter>
+    <div>
+      <Route path="/" component={App}/>
+    </div>
+  </HashRouter>
+), document.getElementById('root'));
 registerServiceWorker();
