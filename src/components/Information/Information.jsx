9477694440ae8e5f3cada62f3755d727a24c5048
akgoode <akgoode@gmail.com>
2017-07-29 20:39:53 -0500

    added content for information and made some style changes

--- src/components/Information/Information.jsx
+++ src/components/Information/Information.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import './Information.css';
+import labels from '../../labels/sitecontent.json';
 
 export class Information extends Component {
   render () {
+    const dealers = labels.dealers;
+    const dealerList = dealers.map((dealer, index) => {
+      return <li key={index}>{dealer.name}</li>
+    });
     return (
-      <div>
-        <h1>Information!</h1>
+      <div className="titlebody">
+        <div className="infoblock infoleft">
+          <p>{labels.collectionInfo}</p>
+        </div>
+        <div className="infoblock inforight">
+          <ul>
+            {dealerList}
+          </ul>
+        </div>
       </div>
     );
   }
