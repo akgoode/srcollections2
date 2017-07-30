6c742250fe84007b9c5eb978c0cf66c403e63141
akgoode <akgoode@gmail.com>
2017-07-30 00:29:46 -0500

    switched to browser router to remove the hash

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Navbar } from './components/Navbar/Navbar.jsx';
 import { Pagetitle } from './components/Pagetitle/Pagetitle.jsx';
-import { Titlebody } from './components/Titlebody/Titlebody.jsx';
 import { Pagefooter } from './components/Pagefooter/Pagefooter.jsx';
 import { Main } from './components/Main/Main.jsx';
 import './App.css';
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { HashRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>
+  </BrowserRouter>
 ), document.getElementById('root'));
 registerServiceWorker();
